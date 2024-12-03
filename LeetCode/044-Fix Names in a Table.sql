ef175047cd7f46b49348bf8a5d331ca7c8c49102
IcyDrae <35934418+IcyDrae@users.noreply.github.com>
2024-12-03 01:57:27 +0100

    add solution: Fix Names in a Table

---
+++ LeetCode/044-Fix Names in a Table.sql
@@ -0,0 +1,53 @@
+/*
+Link: https://leetcode.com/problems/fix-names-in-a-table/description/
+
+Description:
+
+Table: Users
+
++----------------+---------+
+| Column Name    | Type    |
++----------------+---------+
+| user_id        | int     |
+| name           | varchar |
++----------------+---------+
+user_id is the primary key (column with unique values) for this table.
+This table contains the ID and the name of the user. The name consists of only lowercase and
+uppercase characters.
+
+Write a solution to fix the names so that only the first character is uppercase and the rest
+are lowercase.
+
+Return the result table ordered by user_id.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Users table:
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | aLice |
+| 2       | bOB   |
++---------+-------+
+Output: 
++---------+-------+
+| user_id | name  |
++---------+-------+
+| 1       | Alice |
+| 2       | Bob   |
++---------+-------+
+*/
+
+-- Solution
+-- 1. Make only the first character uppercase, the other ones lowercase
+SELECT
+    [user_id],
+    CONCAT(
+        UPPER(SUBSTRING([name], 1, 1)),
+        LOWER(SUBSTRING([name], 2, LEN([name])))
+    ) AS [name]
+FROM [Users]
+ORDER BY [user_id];
